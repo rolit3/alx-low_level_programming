c5cf31177455222928180dbc037116fb99f2c376
rolit3 <hledrurolin@gmail.com>
2022-12-20 15:06:49 -0500

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	} else if (n == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	} else if (n > 5)
+	} else
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
