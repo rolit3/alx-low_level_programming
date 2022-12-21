94a6fda72d8230429714da020ae4a79146f4d2be
rolit3 <hledrurolin@gmail.com>
2022-12-21 13:01:55 -0500

    C programming, lets go !

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdlin.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ *  Return: Always 0
+*/ 
+int int main(void)
+{
+	
+	return (0);
+}
