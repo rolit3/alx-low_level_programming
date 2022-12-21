bcbd6cd00b852083290a59c802290219d4f37647
rolit3 <hledrurolin@gmail.com>
2022-12-20 19:05:47 -0500

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry
+* This program prinnt all alphabet letters in lower and upper case collapsed
+* Return: Always 0
+*/
+
+int main(void)
+{
+	for (int i = 97; i < 122; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
