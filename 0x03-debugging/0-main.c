352c2d4cf53d5f82046709e5f48d00796ec38c52
rolit3 <hledrurolin@gmail.com>
2023-01-03 11:43:09 -0500

    In most projects, we often give you only one main file to test with. For example, this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project:

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * main - main
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-int i;
-i = 0;
+	int i;
 
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
-
+	return (0);
 }
