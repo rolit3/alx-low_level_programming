1a4433946c8cf53df64db316e892eb1372bb1cf2
rolit3 <hledrurolin@gmail.com>
2023-01-04 15:08:07 -0500

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- *
  * @s: char to check
  * Description: this will return the length of a string
  * Return: 0 is success
  */
-int _strlen(char *s)
+int _strlen(char *str)
 {
-int a = 0;
+	int a = 0;
 
-for (; *s++;)
-a++;
-return (a);
-}
+	for (; *str++;)
+		a++;
+	ret
+	
