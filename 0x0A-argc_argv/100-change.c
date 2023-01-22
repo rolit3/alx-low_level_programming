8f16ad50a79bf86b9fa4516aec9d21efc634350a
rolit3 <hledrurolin@gail.com>
2023-01-22 06:02:03 -0500

    Write a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,7 @@
 
 /**
 * main - prints the minimum number of coins to make change for
-* an amount of money
+* an amount of money.
 * @argc: number of arguments
 * @argv: array of arguments
 * Return: returns 1 if there is an error; else returns 0
